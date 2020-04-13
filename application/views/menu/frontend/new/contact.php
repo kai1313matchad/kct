bc15e54f8887ceddeb72b40ba61f23db2f5cfdd8
kai1313matchad <kaishasatrio@match-advertising.com>
2020-04-13 16:00:22 +0700

    update tema baru

--- application/views/menu/frontend/new/contact.php
+++ application/views/menu/frontend/new/contact.php
@@ -25,7 +25,7 @@
 					  <div class="col-sm-12 col-md-12">
 						  <div class="mapouter">
 							  <div class="gmap_canvas maps">
-								  <iframe id="gmap_canvas" src="https://maps.google.com/maps?q=bukakreasi&t=&z=13&ie=UTF8&iwloc=&output=embed"></iframe>
+								  <iframe id="gmap_canvas" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d989.2568765977809!2d112.69979882914849!3d-7.350807669585458!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zN8KwMjEnMDIuOSJTIDExMsKwNDInMDEuMyJF!5e0!3m2!1sen!2sid!4v1586767817822!5m2!1sen!2sid"></iframe>
 							  </div>
 						  </div>
 					  </div>
